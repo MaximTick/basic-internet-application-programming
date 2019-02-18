f30c0bef31a1d9d13c7b02dcfbcc39b5dca74b66
Maxim <maxim.tickhonovich@gmail.com>
2019-02-18 19:08:20 +0300

    lab02

---
+++ WEB02TMA/WEB02TMA/GetTMA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB02TMA
+{
+    public class GetTMA : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            response.AddHeader("X-TMA", "Tikhonovish Maxim");
+            response.WriteFile("response_page.html");
+            response.Write("<h1>" + request.Params["getTMAparam"] + "</h1>");
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
---
+++ WEB02TMA/WEB02TMA/PostTMA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB02TMA
+{
+    public class PostTMA : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            response.Write("radio=" + request.Params["radio"] + "<br>" +
+                "check=" + request.Params["check"] + "<br>" +
+                "text=" + request.Params["text"] + "<br>" +
+                "but2=" + request.Params["but2"] + "<br>" +
+                "button=" + request.Params["button"] + "<br>" + "<br>");
+
+            string[] keys = request.Headers.AllKeys;
+
+            response.Write("<h1> Headers </h1> </br>");
+            foreach(var key in keys)
+            {
+                response.Write(key + " = " + request.Headers[key] + "<br>");
+            }
+
+            string[] parmKeys = request.Params.AllKeys;
+            response.Write("<h1> PARAMS </h1> <br>");
+
+            foreach(var key in parmKeys)
+            {
+                response.Write(key + " = " + request.Params[key] + "<br>");
+            }
+        }
+    }
+}
