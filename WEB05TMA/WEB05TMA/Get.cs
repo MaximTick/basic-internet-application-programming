da328eebe59589df1be30f86a223764c05b994f1
Maxim <maxim.tickhonovich@gmail.com>
2019-03-26 22:50:37 +0300

    Basic Authenticate

---
+++ WEB05TMA/WEB05TMA/Get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB05TMA
+{
+    public class Get : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest req = context.Request;
+            HttpResponse res = context.Response;
+            res.StatusCode = 401;
+            res.AddHeader("WWW-Authenticate", "Basic");
+            res.End();
+        }
+    }
+}
