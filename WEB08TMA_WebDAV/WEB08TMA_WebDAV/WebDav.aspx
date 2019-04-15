83a255845090656ca39460e0ecab34a85c207bcc
Максим Тихонович <maxim.tickhonovich@gmail.com>
2019-04-15 12:09:39 +0300

    WebDAV

---
+++ WEB08TMA_WebDAV/WEB08TMA_WebDAV/WebDav.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebDav.aspx.cs" Inherits="WEB08TMA_WebDAV.WebDav" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" ID ="createDirectory" Text="createDirectory" OnClick="createDirectory_Click" />
+            <asp:Button runat="server" ID ="delDir" Text="deleteDir" OnClick="deleteDirectory_Click" />
+            <asp:Button runat="server" ID ="Button1" Text="deleteFile" OnClick="deleteFile_Click" />
+            <asp:Button runat="server" ID ="Button2" Text="download" OnClick="downloadFile_Click" />
+            <asp:Button runat="server" ID ="Button3" Text="copy" OnClick="CopyFile_Click" />
+            <asp:Button runat="server" ID ="Button4" Text="upload" OnClick="uploadFile_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WEB08TMA_WebDAV/WEB08TMA_WebDAV/WebDav.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEB08TMA_WebDAV
+{
+    public partial class WebDav : System.Web.UI.Page
+    {
+        #region Password
+
+        string emailOpenDrive = "maxim.tickhonovich@gmail.com";
+        string passwordOpenDrive = "";
+        #endregion Password
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void createDirectory_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = HttpWebRequest.Create("https://webdav.opendrive.com/maxik") as HttpWebRequest;
+            request.Credentials = new NetworkCredential("maxim.tickhonovich@gmail.com", "qwert12345");
+            request.Method = WebRequestMethods.Http.MkCol;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            HttpStatusCode code = response.StatusCode;
+        }
+
+        protected void deleteDirectory_Click(object sender, EventArgs e)
+        {
+            //https://webdav.yandex.ru/maximka
+            HttpWebRequest request = HttpWebRequest.Create("https://webdav.opendrive.com/maxik") as HttpWebRequest;
+            request.Credentials = new NetworkCredential(emailOpenDrive, passwordOpenDrive);
+            request.Method = "DELETE";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            HttpStatusCode code = response.StatusCode;
+        }
+
+        protected void deleteFile_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = HttpWebRequest.Create("https://webdav.opendrive.com/sert.jpg") as HttpWebRequest;
+            request.Credentials = new NetworkCredential(emailOpenDrive, passwordOpenDrive);
+            request.Method = "DELETE";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            HttpStatusCode code = response.StatusCode;
+        }
+
+        protected void downloadFile_Click(object sender, EventArgs e)
+        {
+            string url = "https://webdav.opendrive.com/test.docx";
+            string save_path = "D:\\";
+            string name = "Тестовый файл.docx";
+
+            WebClient wc = new WebClient();
+            wc.Credentials = new NetworkCredential(emailOpenDrive, passwordOpenDrive);
+            wc.DownloadFile(url, save_path + name);
+        }
+
+        protected void uploadFile_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://webdav.opendrive.com/1/my.txt");
+            req.Credentials = new NetworkCredential(emailOpenDrive, passwordOpenDrive);
+            req.Accept = "*/*";
+            req.Headers.Add("Depth: 1");
+            req.Method = "PUT";
+            req.ContentType = "application/binary";
+            using (Stream myReqStream = req.GetRequestStream())
+            {
+                using (FileStream myFile = new FileStream("E:\\lol.txt", FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader myReader = new BinaryReader(myFile))
+                    {
+                        byte[] buffer = myReader.ReadBytes(2048);
+                        while (buffer.Length > 0)
+                        {
+                            myReqStream.Write(buffer, 0, buffer.Length);
+                            buffer = myReader.ReadBytes(2048);
+                        }
+                    }
+                }
+            }
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+        }
+
+        protected void CopyFile_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest req = HttpWebRequest.Create("https://webdav.opendrive.com/1/sert.jpg") as HttpWebRequest;
+            req.Credentials = new NetworkCredential(emailOpenDrive, passwordOpenDrive);
+            req.Method = "COPY";
+            req.Headers.Add("Destination", "https://webdav.opendrive.com/2/copy.jpg");
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            HttpStatusCode code = res.StatusCode;
+        }
+
+    }
+}
